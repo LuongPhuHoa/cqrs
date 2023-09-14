265b663d55448d93c5b9cada7e2074dbac06575e
LuongPhuHoa <36793008+LuongPhuHoa@users.noreply.github.com>
2023-09-14 14:36:48 +0700

    update-unit-test

--- src/post/post.controller.spec.ts
+++ src/post/post.controller.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostController } from './post.controller';
+import { PostService } from './post.service';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 
 describe('PostController', () => {
   let controller: PostController;
+  let commandBus: CommandBus;
+  let queryBus: QueryBus;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PostService,
+        { provide: CommandBus, useValue: { execute: jest.fn() } },
+        { provide: QueryBus, useValue: { execute: jest.fn() } },
+      ],
       controllers: [PostController],
     }).compile();
 
     controller = module.get<PostController>(PostController);
+    commandBus = module.get<CommandBus>(CommandBus);
+    queryBus = module.get<QueryBus>(QueryBus);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  //test post request
+  it('POST: should return post', async () => {
+    const mockPost = { title: 'test', content: 'test content' };
+    jest.spyOn(commandBus, 'execute').mockResolvedValue(mockPost);
+
+    const post = await controller.createPost(mockPost);
+    expect(post).toEqual(mockPost);
+  });
+
+  //test get request
+  it('GET: should return post', async () => {
+    const mockPost = { id: 1, title: 'test', content: 'test content' };
+    jest.spyOn(queryBus, 'execute').mockResolvedValue(mockPost);
+
+    const post = await controller.getPost(1);
+    expect(post).toEqual(mockPost);
   });
 });
--- src/post/post.service.spec.ts
+++ src/post/post.service.spec.ts
@@ -12,7 +12,33 @@ describe('PostService', () => {
     service = module.get<PostService>(PostService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  //test create
+  it('should return post', async () => {
+    const post = await service.create({
+      id: 1,
+      title: 'test',
+      content: 'test content',
+    });
+    expect(post).toEqual({
+      id: 1,
+      title: 'test',
+      content: 'test content',
+    });
+  });
+
+  //test findById
+  it('should return post', async () => {
+    await service.create({
+      id: 1,
+      title: 'test',
+      content: 'test content',
+    });
+
+    const foundPost = await service.findById(1);
+    expect(foundPost).toEqual({
+      id: 1,
+      title: 'test',
+      content: 'test content',
+    });
   });
 });
